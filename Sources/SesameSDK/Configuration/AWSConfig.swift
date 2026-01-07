63e26fc946b88398882bdd30dd27e817d47a3fda
frey <shenfei@cn.candyhouse.co>
2026-01-07 21:10:03 +0800

    Sync from private repository add awsconfig

---
+++ Sources/SesameSDK/Configuration/AWSConfig.swift
@@ -0,0 +1,15 @@
+//
+//  AWSConfig.swift
+//  SesameSDK
+//
+//  Created by frey Mac on 2026/1/7.
+//
+
+public struct AWSConfig {
+    // AWS API
+    public static let apiKey = "iGgXj9GorS4PeH90mAysg1l7kdvoIPxM25mPFl3k"
+    public static let clientId = "ap-northeast-1:0a1820f1-dbb3-4bca-9227-2a92f6abf0ae"
+    
+    // AWS IoT
+    public static let iotEndpoint = "https://d06107753ay3c67v7y9pa-ats.iot.ap-northeast-1.amazonaws.com"
+}
